48fe42e67deb2d591c5134ce7a17d4f2075c23eb
jiljith <sruthijiljith@gmail.com>
2019-12-06 18:25:57 +0530

    commit 3

--- Hello.java
+++ Hello.java
@@ -2,10 +2,18 @@ class Hello
 {
 public static void main(String args[])
 {
+int hello=0;
 System.out.println("Hello");
 
 System.out.println("*****************************");
 System.out.println("#############################");
-
+hello=4;
+if(hello<5)
+{
+System.out.println("hello this inside if ");
+}else
+{
+System.out.println("hello this is inside else");
+}
 }
 }
