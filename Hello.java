f7166d5610ca4a3f0ef61e67ee5e5ce9e2d16da5
jiljith <sruthijiljith@gmail.com>
2019-12-06 17:57:08 +0530

    modified

--- Hello.java
+++ Hello.java
@@ -3,5 +3,9 @@ class Hello
 public static void main(String args[])
 {
 System.out.println("Hello");
+
+System.out.println("*****************************");
+System.out.println("#############################");
+
 }
 }
